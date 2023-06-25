5359f824678f993a17e960b3db6a41deb1ed9f69
sultaAann <sultandjavgashev@gmail.com>
2023-06-26 02:10:54 +0500

    ok

--- task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
+++ task1-proxy-pattern/src/main/java/com/mjc/stage2/impl/ProxyConnection.java
@@ -11,7 +11,7 @@ public class ProxyConnection implements Connection {
     }
 
     public void reallyClose() {
-        if (!isClosed()) realConnection.close();
+        realConnection.close();
     }
 
     @Override
@@ -24,8 +24,6 @@ public class ProxyConnection implements Connection {
 
     @Override
     public boolean isClosed() {
-        if (realConnection.isClosed() == true) return true;
-        return false;
+        return realConnection.isClosed();
     }
-
 }
